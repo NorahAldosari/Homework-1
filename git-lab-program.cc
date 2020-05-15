7d0e512084c081ff1cf8b4f6cbf4acade92ccb67
Norah Aldosari <na314617@ohio.edu>
2020-05-15 02:37:03 -0700

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Norah Aldosari
+ *        Date: MAy 15, 2020
+ * Description: first homework program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
